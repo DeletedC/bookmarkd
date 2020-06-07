a70caad98d7cf7639dc14409989acea85e329c64
Curtis <CurtisWoodsArt@gmail.com>
2020-06-07 12:50:15 -0400

    Created function to get the bookmarks from the API and display on the page

--- bookmarkd_front/src/App/index.js
+++ bookmarkd_front/src/App/index.js
@@ -3,11 +3,39 @@ import "./style.css";
 
 const App = (props) => {
 
+  // State to hold the bookmarks
+  const [books, setBooks] = React.useState(null);
+
+  // Get the bookmarks from the API
+  const getInfo = async () => {
+    const response = await fetch('http://localhost:3001/books');
+    const result = await response.json();
+    console.log(result);
+    setBooks(result);
+  };
+
+  // Hook to get the bookmarks when the component loads
+  React.useEffect(() => {
+    getInfo();
+  }, []);
 
 
   return (
     <>
       <h1>Bookmarkd</h1>
+      <ul>
+        {books
+          ? books.map((bookmark) => {
+            return (
+              <li>
+                <h2>{bookmark.title}</h2>
+                <h2>{bookmark.url}</h2>
+              </li>
+            );
+          })
+          : "Loading..."
+        }
+      </ul>
     </>
   );
 }
