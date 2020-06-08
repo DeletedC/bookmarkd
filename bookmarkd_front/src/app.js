cf9d58bb3879e39dd91c8eba0787ee1134f505d5
Curtis <CurtisWoodsArt@gmail.com>
2020-06-07 21:47:31 -0400

    Changed buttons from X and E to Delete and Edit

--- bookmarkd_front/src/app.js
+++ bookmarkd_front/src/app.js
@@ -91,10 +91,10 @@ const App = (props) => {
               <li key={bookmark._id}>
                 <a href={bookmark.url}><h5>{bookmark.title}</h5></a>
                 <button class="btn btn-link" onClick={() => {handleDelete(bookmark._id);}}>
-                  X
+                  Delete
                 </button>
                 <button class="btn btn-link" onClick={() => {handleSelect(bookmark)}}>
-                  E
+                  Edit
                 </button>
               </li>
             );
